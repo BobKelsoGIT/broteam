8b7a1d0efcbcf92ab9c124ba17a78f83e34635a9
Ivan Scherbakov <lolwut155@gmail.com>
2024-09-15 22:17:44 +0300

    hosts

--- brotweaks/settings.py
+++ brotweaks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.twc1.net']
 
 # Application definition
 
